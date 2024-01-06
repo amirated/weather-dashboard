a97c3e39cec11e926e3628709e06596bdaf83884
amirated <ratedami@gmail.com>
2024-01-06 16:43:10 +0530

    integration to openweather api using axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import InputText from './components/InputText';
 
 function App() {
+  const [data, setData] = useState({});
+  
+  const searchLocation = (location: string) => {
+    const url = `${process.env.REACT_APP_OPENWEATHER_API_ENDPOINT}?q=${location}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`
+    axios.get(url)
+      .then((response) => {
+        setData(response.data);
+      });
+  }
   return (
-    <div className="App">
-      <InputText placeholder={'Type the city name here.'} />
+    <div className="w-full h-full relative">
+      <div className="text-center p-4">
+        <InputText
+          placeholder={'Type the city name here.'}
+          handleEnter={searchLocation}
+         />
+      </div>
     </div>
   );
 }
--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -1,16 +1,28 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 interface InputTextProps {
   placeholder: string;
+  handleEnter: any;
 }
 
-const InputText: FC<InputTextProps> = ({ placeholder }) => {
+const InputText: FC<InputTextProps> = ({ placeholder, handleEnter }) => {
+  const [value, setValue] = useState("");
+  const handleKeyDown = (event: {key: string;}) => {
+    if (event.key === "Enter") {
+      handleEnter(value);
+      setValue("");
+    }
+  }
   return (
     <>
       <input 
-      type="text"
-      className='py-3 px-6 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400 focus:outline-none bg-white-600/100 shadow-md'
-      placeholder={placeholder} />
+        type="text"
+        className='py-3 px-6 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400 focus:outline-none bg-white-600/100 shadow-md'
+        placeholder={placeholder}
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+        onKeyDownCapture={(event) => handleKeyDown(event)}
+      />
     </>
   );
 };
